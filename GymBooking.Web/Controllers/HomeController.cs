7b90bf9b1ffda1152eeb4fa915186043aac06ee7
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-03-08 15:03:40 +0100

    Get info from ClaimsPrincipal with UserManager

--- GymBooking.Web/Controllers/HomeController.cs
+++ GymBooking.Web/Controllers/HomeController.cs
@@ -1,20 +1,28 @@
 ﻿using GymBooking.Web.Models;
+using GymBooking.Web.Models.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace GymBooking.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            this.userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            //var user = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            //var user2 = await userManager.GetUserAsync(User);
+            //var userId =  userManager.GetUserId(User);
             return View();
         }
 
