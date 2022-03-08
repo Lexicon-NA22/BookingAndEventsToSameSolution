2f3e6b3e5c247bfd9090e76e240284bccf6d7554
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-03-08 13:18:12 +0100

    Add Entities

---
+++ GymBooking.Web/Models/Entities/GymClass.cs
@@ -0,0 +1,14 @@
+﻿namespace GymBooking.Web.Models.Entities
+{
+    public class GymClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Description { get; set; }
+        public DateTime EndTime => StartDate + Duration;
+
+        ICollection<ApplicationUserGymClass> AttendingMembers { get; set; }
+    }
+}
