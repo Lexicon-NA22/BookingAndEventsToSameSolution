8bd2fd62cf1f9876265be9798f09650ab9138622
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-03-09 13:42:17 +0100

    BaseClient

---
+++ GymBooking.Web/Clients/BookingClient.cs
@@ -0,0 +1,14 @@
+﻿using GymBooking.Web.Models.Entities;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace GymBooking.Web.Clients
+{
+    public class BookingClient : BaseClient
+    {
+        public BookingClient(HttpClient httpClient) : base(httpClient, new Uri(""), "application/json")
+        { }
+
+       
+    }
+}
