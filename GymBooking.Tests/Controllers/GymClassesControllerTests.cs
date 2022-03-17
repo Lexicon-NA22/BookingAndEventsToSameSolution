35cda7ac83a7c8b090357835b69f62a32125f8d5
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-03-17 13:07:33 +0100

    SetUp Tests

---
+++ GymBooking.Tests/Controllers/GymClassesControllerTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace GymBooking.Tests.Controllers
+{
+
+    [TestClass]
+    public class GymClassesControllerTests
+    {
+        [TestInitialize]
+        public void TestSetUp()
+        {
+
+        }
+
+        [TestMethod]
+        public async Task Index_NotAuthorized_ShouldReturnIndexViewModel()
+        {
+
+        }
+
+    }
+}
---
+++ GymBooking.Tests/SetUp/GlobalSetUp.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymBooking.Tests.SetUp
+{
+    public class GlobalSetUp
+    {
+        [AssemblyInitialize]
+        public static void AssemblySetUp(TestContext context)
+        {
+            context.WriteLine("Assembley init start");
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanUp()
+        {
+
+        }
+    }
+}
